176c24dfe280fad90089236cefcf0ecd5bf1e9d5
Pablo ( Paulo )  Lopez <pau_lo_lop_ez@protonmail.com>
2020-04-11 17:08:54 -0700

    created files for pages app and password_change_form.html

--- listing/settings.py
+++ listing/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
